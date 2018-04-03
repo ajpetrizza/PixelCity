541ca30f0c03dac23186942ccb8e344e51d95313
ajpetrizza <ajpetrizza@fullsail.edu>
2018-04-03 19:29:38 -0400

    Init commit

---
+++ Pixel-city/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  Pixel-city
+//
+//  Created by Anthony Petrizza on 3/29/18.
+//  Copyright © 2018 Anthony Petrizza. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
